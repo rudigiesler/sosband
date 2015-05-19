9d54f004037d513859a0d5676054f5d6708deabe
Rudi Giesler <rudi@praekelt.com>
2015-05-19 14:27:48 +0200

    Add migrations for new models

--- api_server/migrations/versions/3eaa2f19084e_.py
+++ api_server/migrations/versions/3eaa2f19084e_.py
@@ -1,13 +1,13 @@
 """empty message
 
-Revision ID: 5ac12761d383
+Revision ID: 3eaa2f19084e
 Revises: None
-Create Date: 2015-05-19 11:47:33.509635
+Create Date: 2015-05-19 13:43:31.698296
 
 """
 
 # revision identifiers, used by Alembic.
-revision = '5ac12761d383'
+revision = '3eaa2f19084e'
 down_revision = None
 
 from alembic import op
