4d5236cc9024dcf97284af308d92b7130af4d144
Rudi Giesler <rudi@praekelt.com>
2015-05-19 14:28:13 +0200

    Add management command for creating priority levels

--- api_server/manage.py
+++ api_server/manage.py
@@ -3,12 +3,21 @@ from flask.ext.migrate import Migrate, MigrateCommand
 import os
 
 from app import app, db
+from models import NumberPriorities
 app.config.from_object(os.environ['APP_SETTINGS'])
 
 migrate = Migrate(app, db)
 manager = Manager(app)
 
 manager.add_command('db', MigrateCommand)
+
+@manager.command
+def create_priorities(levels=2):
+    "Create phone number priorities"
+    for i in range(levels):
+        p = NumberPriorities(i)
+        db.session.add(p)
+    db.session.commit()
 
 if __name__ == '__main__':
     manager.run()
