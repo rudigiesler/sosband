4936463256b705ef5e854b620256e81eec92b3a5
Rudi Giesler <rudi@praekelt.com>
2015-05-21 10:28:59 +0200

    Fix arduino numbers endpoint

--- api_server/app.py
+++ api_server/app.py
@@ -15,7 +15,7 @@ from models import NumberPriorities, EmergencyNumbers
 def hello():
     return "Hello World!"
 
-@app.route('/emergency_numbers')
+@app.route('/emergency_numbers', methods=['GET'])
 def emergency_numbers():
     def serialize(number):
         ret = {}
@@ -38,19 +38,41 @@ def emergency_numbers():
     numbers = [serialize(n) for n in numbers]
     return json.dumps(numbers)
 
-@app.route('/arduino/numbers')
+@app.route('/priorities/<int:priority>', methods=['GET', 'POST'])
+def priorities_resource(priority):
+    if request.method == "GET":
+        return get_number_for_priority(priority)
+    else:
+        return set_number_for_priority(priority)
+
+
+def get_number_for_priority(priority):
+    """ Returns the current emergency number for the priority """
+    pass
+
+
+def set_number_for_priority(priority):
+    """ Archives the old emergency number, and creates a new emergency number
+        with the priority `priority`. """
+    pass
+
+@app.route('/arduino/numbers',  methods=['GET'])
 def arduino_numbers():
     """ A special endpoint for the Arduino designed to be easy to process
         on the Arduino """
-    priorities = (NumberPriorities.query
-        .outerjoin(EmergencyNumbers)
-        .order_by(NumberPriorities.priority.asc())
+    numbers = (
+        db.session.query(NumberPriorities, EmergencyNumbers.number)
+        .outerjoin(EmergencyNumbers, db.and_(
+            EmergencyNumbers.priority == NumberPriorities.priority,
+            EmergencyNumbers.archived == False))
+        .order_by(NumberPriorities.priority)
         )
-    numbers = []
-    for n in priorities:
-        if len(n.numbers) > 0:
-            numbers.append(n.numbers[0].number)
-        else:
-            numbers.append('')
-    print '\n'.join(numbers)
+    numbers = [n or '' for p, n in numbers]
+
     return '\n'.join(numbers)
+
+@app.route('/arduino/gps', methods=['POST'])
+def arduino_gps_data():
+    """ A special endpoint for the Arduino designed to be very easy
+        to post NMEA data directly to """
+    
